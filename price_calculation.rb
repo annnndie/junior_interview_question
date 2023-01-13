7057840ece02ec950ae67b6d5e0f0f232b045ec0
Roy Huang <beersheep0109@gmail.com>
2023-01-13 21:56:19 +0800

    Update price_calculation.rb
    Fix Typo

--- price_calculation.rb
+++ price_calculation.rb
@@ -7,16 +7,38 @@ require './models/order.rb'
 # 2. 若消費期間有超過兩個優惠活動，取最優者折扣 
 # 3. 運費計算在優惠折抵之後
 #
-# Please implmenet the following methods 
+# Please implemenet the following methods.
 # Additional helper methods are recommended.
 
 class PriceCalculation
+  FREE_SHIPMENT_PRICE = 1500
+  SHIPPING_FEE = 60
+
   def initialize(order_id)
+    @order = Order.find(order_id)
+    raise Order::NotFound if @order.nil?
   end
 
   def total
+    @total ||= apply_total_price
   end
 
   def free_shipment?
+    @order.price >= FREE_SHIPMENT_PRICE
+  end
+
+  private
+
+  def apply_total_price
+    price = calculate_price_with_campaign
+    free_shipment? ? price : price + SHIPPING_FEE
+  end
+
+  def best_discount_ratio
+    @best_discount_ratio ||= Campaign.running_campaigns(@order.order_date).max_by(&:discount_ratio)&.discount_ratio || 0
+  end
+
+  def calculate_price_with_campaign
+    @order.price * (1 - best_discount_ratio / 100.0) 
   end
 end
